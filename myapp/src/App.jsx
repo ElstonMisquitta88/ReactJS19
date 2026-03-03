bbc0b532f453778ad0a649fe9b3bd7b1a9317efe
Elston Misquitta <elston007@gmail.com>
2026-03-03 23:22:16 +0530

    Changes

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,13 +1,12 @@
-import Databinding from "./Databinding";
+import Databinding from "./components/Databinding";
 
 // This is a component file which is used to create a component in react
 
 function App() {
   return (
    <>
-    <h1>Hello World </h1>
     <h3>Welcome to my React App</h3>
-    <h4>Hello Elston ! This is my First React App </h4>
+    <br/><br/><br/>
    <Databinding />
    </>
   )
