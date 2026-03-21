8508ddf83adbb23f695fc43a15d3acda4f7d7163
Elston Misquitta <elston007@gmail.com>
2026-03-21 11:16:19 +0530

    React JS Study Changes

---
+++ myapp/mutuable.js
@@ -0,0 +1,3 @@
+const obj={personid :1,personname :"sachin",personage: 30}
+obj.personage=31
+console.log(obj)
---
+++ myapp/src/components/UnControlledForm.jsx
@@ -0,0 +1,9 @@
+
+
+function UnControlledForm() {
+ 
+    
+    return(
+        <></>
+    )
+}
